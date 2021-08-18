cf16632e06c2c1d9a91c24eb3cc06e9e5474d8c2
Dhanashree Rane <dhanashreerane18@gmail.com>
2021-08-18 20:36:24 +0530

    Completed

--- Binary-Veda-Assignments/app/src/main/java/com/masai/binaryvedaassignment/LandingPageActivity.kt
+++ Binary-Veda-Assignments/app/src/main/java/com/masai/binaryvedaassignment/LandingPageActivity.kt
@@ -1,11 +1,18 @@
 package com.masai.binaryvedaassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnRegester.setOnClickListener {
+            val intent = Intent(this,ProfilePageActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
